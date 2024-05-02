1926b2d40ba78b9776b3f57abc12c731a7812a75
Barathi <barathiofficial@gmail.com>
2024-05-03 00:15:44 +0530

    expo init

--- web/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app'
-
-const container = document.getElementById('app')
-const root = ReactDOM.createRoot(container!)
-
-root.render(<App />)
