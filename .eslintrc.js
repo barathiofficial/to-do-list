97f1cc6dda6602ce994aee6428f50a19877972f5
Barathi <barathiofficial@gmail.com>
2024-05-08 12:05:53 +0530

    chore: Update ESLint configuration with prettier plugin and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
 	env: {
 		browser: true,
 		es2021: true
 	},
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended'
+	],
 	overrides: [
 		{
 			env: {
@@ -20,11 +29,59 @@ module.exports = {
 		ecmaVersion: 'latest',
 		sourceType: 'module'
 	},
-	plugins: ['@typescript-eslint', 'react'],
+	plugins: ['@typescript-eslint', 'react', 'prettier'],
 	rules: {
-		indent: ['error', 'tab'],
+		indent: ['error', 'tab', { SwitchCase: 1 }],
 		'linebreak-style': ['error', 'windows'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'never']
+		quotes: ['off', 'single'],
+		semi: ['error', 'never'],
+		'@typescript-eslint/consistent-type-imports': [
+			'error',
+			{
+				prefer: 'type-imports',
+				disallowTypeAnnotations: true,
+				fixStyle: 'separate-type-imports'
+			}
+		],
+		'react/no-unescaped-entities': 'error',
+		'no-console': 'error',
+		'padding-line-between-statements': [
+			'warn',
+			{ blankLine: 'always', prev: '*', next: 'block' },
+			{ blankLine: 'always', prev: 'block', next: '*' },
+			{ blankLine: 'always', prev: '*', next: 'block-like' },
+			{ blankLine: 'always', prev: 'block-like', next: '*' }
+		],
+		'@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+		'react/jsx-sort-props': [
+			'error',
+			{
+				callbacksLast: true,
+				shorthandFirst: true,
+				shorthandLast: false,
+				multiline: 'last',
+				ignoreCase: true,
+				noSortAlphabetically: false,
+				reservedFirst: true,
+				locale: 'auto'
+			}
+		],
+		'no-empty': 'off',
+		'prettier/prettier': [
+			'error',
+			{},
+			{
+				usePrettierrc: true
+			}
+		],
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector:
+					'ArrowFunctionExpression:not(CallExpression > ArrowFunctionExpression):not(Property > ArrowFunctionExpression):not(JSXExpressionContainer > ArrowFunctionExpression)',
+				message:
+					'Arrow functions are not allowed except in callbacks, object properties, and JSX properties.'
+			}
+		]
 	}
 }
