5583ce30d44b0dc0eb8229b6cf1c28c58d6d957c
Barathi <barathiofficial@gmail.com>
2024-05-12 13:11:02 +0530

    refactor: Update store middleware to ignore thunk actions in serializableCheck

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit'
 import reducer from './slices'
+import * as thunks from './thunks'
 
 const store = configureStore({
-	reducer
+	reducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [
+					...Object.values(thunks).reduce<string[]>((acc, thunk) => {
+						return [
+							...acc,
+							thunk.fulfilled.type,
+							thunk.pending.type,
+							thunk.rejected.type
+						]
+					}, [])
+				]
+			}
+		})
 })
 
 export default store
