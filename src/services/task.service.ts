811112ad273f9b2de94ce91e7976ae1fda0cca00
Barathi <barathiofficial@gmail.com>
2024-05-12 13:01:15 +0530

    refactor: Update task service to use prepared statements for database operations

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,72 +1,118 @@
-import { execute } from '@lib/db'
+import initDB from '@lib/db'
 
 export type Task = {
-	id?: number
-	text?: string
-	completed?: boolean
+	id: number
+	text: string
+	completed: boolean
 }
 
-export async function fetchTasks() {
+type RawTask = {
+	id: number
+	text: string
+	completed: number
+}
+
+export async function $addTask(text: string) {
+	const db = await initDB()
+	const query = 'INSERT INTO `tasks` (`text`, `completed`) VALUES ($text, $completed)'
+	const stmt = await db.prepareAsync(query)
+
+	try {
+		const result = await stmt.executeAsync({ $text: text, $completed: 0 })
+
+		return {
+			id: result.lastInsertRowId,
+			text,
+			completed: false
+		}
+	} catch (e) {
+	} finally {
+		await stmt.finalizeAsync()
+	}
+}
+
+export async function $fetchTasks() {
+	const db = await initDB()
 	const query = 'SELECT * FROM `tasks` ORDER BY `completed` ASC, `id` DESC'
-	const result = await execute(query)
-	const tasks: Task[] = []
+	const stmt = await db.prepareAsync(query)
 
-	for (let i = 0; i < result.rows.length; i++) {
-		const row = result.rows.item(i)
-		tasks.push({
-			id: row.id,
-			text: row.text,
-			completed: row.completed === 1
+	try {
+		const result = await stmt.executeAsync<RawTask>()
+		const tasks = await result.getAllAsync()
+
+		return tasks.map<Task>((task) => ({
+			id: task.id,
+			text: task.text,
+			completed: task.completed === 1
+		}))
+	} catch (e) {
+	} finally {
+		await stmt.finalizeAsync()
+	}
+}
+
+export async function $deleteTask(id: number) {
+	const db = await initDB()
+	const query = 'DELETE FROM `tasks` WHERE `id` = $id'
+	const stmt = await db.prepareAsync(query)
+
+	try {
+		await stmt.executeAsync({ $id: id })
+		return id
+	} catch (e) {
+	} finally {
+		await stmt.finalizeAsync()
+	}
+}
+
+export async function $updateTask(task: Task) {
+	const db = await initDB()
+	const query = 'UPDATE `tasks` SET `text` = $text, `completed` = $completed WHERE `id` = $id'
+	const stmt = await db.prepareAsync(query)
+
+	try {
+		await stmt.executeAsync({
+			$id: task.id,
+			$text: task.text,
+			$completed: task.completed ? 1 : 0
 		})
-	}
 
-	return tasks
-}
-
-export async function addTask(text: string) {
-	if (!text.trim()) {
-		throw new Error('Task text is required')
-	}
-
-	const query = 'INSERT INTO `tasks` (`text`, `completed`) VALUES (?, 0)'
-	const result = await execute(query, [text])
-	return {
-		id: result.insertId,
-		text,
-		completed: false
+		return $fetchTask(task.id)
+	} catch (e) {
+	} finally {
+		await stmt.finalizeAsync()
 	}
 }
 
-export async function toggleTask(id: number) {
-	if (!id) {
-		throw new Error('Task ID is required')
-	}
+export async function $fetchTask(id: number) {
+	const db = await initDB()
+	const query = 'SELECT * FROM `tasks` WHERE `id` = $id'
+	const stmt = await db.prepareAsync(query)
 
-	const query = 'UPDATE `tasks` SET completed = NOT `completed` WHERE `id` = ?'
-	await execute(query, [id])
-	return id
+	try {
+		const result = await stmt.executeAsync<RawTask>({ $id: id })
+		const task = await result.getFirstAsync()
+		return {
+			id: task?.id || 0,
+			text: task?.text || '',
+			completed: task?.completed === 1
+		} as Task
+	} catch (e) {
+	} finally {
+		await stmt.finalizeAsync()
+	}
 }
 
-export async function updateTask(data: Task) {
-	if (!data.id) {
-		throw new Error('Task ID is required')
+export async function $toggleTask(id: number) {
+	const db = await initDB()
+	const query = 'UPDATE `tasks` SET `completed` = NOT `completed` WHERE `id` = $id'
+	const stmt = await db.prepareAsync(query)
+
+	try {
+		await stmt.executeAsync({ $id: id })
+		return $fetchTask(id)
+	} catch (e) {
+	} finally {
+		await stmt.finalizeAsync()
 	}
-
-	if (!data.text && !data.completed) {
-		throw new Error('Task text or completion status is required')
-	}
-
-	const query = 'UPDATE `tasks` SET `text` = ?, `completed` = ? WHERE `id` = ?'
-	await execute(query, [data.text || '', data.completed ? 1 : 0, data.id])
-	return data
-}
-
-export async function deleteTask(id: number) {
-	if (!id) {
-		throw new Error('Task ID is required')
-	}
-
-	const query = 'DELETE FROM `tasks` WHERE `id` = ?'
-	await execute(query, [id])
-	return id
 }
