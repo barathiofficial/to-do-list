4ce2f376eada6313039d894436c975cab7b34b86
Barathi <barathiofficial@gmail.com>
2024-05-13 13:23:07 +0530

    refactor: Update SQLite initialization to use async/await

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,15 @@
 import * as SQLite from 'expo-sqlite'
 
-export default async function initDB() {
-	const db = await SQLite.openDatabaseAsync('todo.db')
+let db: SQLite.SQLiteDatabase | null = null
 
-	db.execAsync(
-		'CREATE TABLE IF NOT EXISTS `tasks` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `text` TEXT NOT NULL, `completed` INTEGER NOT NULL DEFAULT 0)'
-	)
+export default async function initDB() {
+	if (!db) {
+		db = await SQLite.openDatabaseAsync('todo.db')
+
+		db.execAsync(
+			'CREATE TABLE IF NOT EXISTS `tasks` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `text` TEXT NOT NULL, `completed` INTEGER NOT NULL DEFAULT 0)'
+		)
+	}
 
 	return db
 }
