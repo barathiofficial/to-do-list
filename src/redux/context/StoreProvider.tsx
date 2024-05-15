fbd9681fb19a29b3f7874edf7c35cedabde523a4
Barathi <barathiofficial@gmail.com>
2024-05-16 00:02:11 +0530

    refactor: Add StoreProvider component for Redux store management

---
+++ src/redux/context/StoreProvider.tsx
@@ -0,0 +1,7 @@
+import store from '@redux/store'
+import React from 'react'
+import { Provider } from 'react-redux'
+
+export default function StoreProvider(props: React.PropsWithChildren) {
+	return <Provider store={store}>{props.children}</Provider>
+}
