99f8abb7bd1dd0a20a3749a2f71e6d7a3e6766a9
Barathi <barathiofficial@gmail.com>
2024-05-16 00:01:39 +0530

    refactor: Update header component to use dynamic title prop

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,45 +1,16 @@
 import { colors } from '@constants'
-import Feather from '@expo/vector-icons/Feather'
-import { gloablStyles, ripple, sizes, typography } from '@themes'
+import { gloablStyles, sizes, typography } from '@themes'
 import React from 'react'
-import { Animated, Pressable, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 type HeaderProps = {
-	onIconPress?: () => void
-	rotatePlus?: boolean
+	title?: string
 }
 
 export function Header(props: HeaderProps) {
-	const rotateAnim = React.useRef(new Animated.Value(0)).current
-
-	const rotate = rotateAnim.interpolate({
-		inputRange: [0, 1],
-		outputRange: ['0deg', '45deg']
-	})
-
-	React.useEffect(() => {
-		Animated.timing(rotateAnim, {
-			toValue: props.rotatePlus ? 1 : 0,
-			duration: 100,
-			useNativeDriver: true
-		}).start()
-	}, [props.rotatePlus])
-
 	return (
 		<View style={[styles.header, gloablStyles.shadow]}>
-			<Text style={styles.title}>Tasks</Text>
-			<Animated.View style={[gloablStyles.iconWrapper, { transform: [{ rotate }] }]}>
-				<Pressable
-					android_ripple={ripple}
-					style={gloablStyles.iconPressable}
-					onPress={props.onIconPress}>
-					<Feather
-						color={colors.dark}
-						name='plus'
-						size={24}
-					/>
-				</Pressable>
-			</Animated.View>
+			<Text style={typography.lg}>{props.title}</Text>
 		</View>
 	)
 }
@@ -53,6 +24,5 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		paddingLeft: 20,
 		paddingRight: 10
-	},
-	title: typography.lg
+	}
 })
