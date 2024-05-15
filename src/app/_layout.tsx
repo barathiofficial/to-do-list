624155207f74d1810482b2301a4cdb0d1e982997
Barathi <barathiofficial@gmail.com>
2024-05-16 00:02:45 +0530

    refactor: Update _layout component to use StoreProvider for Redux store management

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,24 +1,23 @@
-import { colors } from '@constants'
-import store from '@redux/store'
-import { Slot } from 'expo-router'
+import StoreProvider from '@redux/context/StoreProvider'
+import { Stack } from 'expo-router'
 import React from 'react'
 import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Provider } from 'react-redux'
 
-export default function Layout() {
+export default function AppLayout() {
 	return (
-		<Provider store={store}>
-			<SafeAreaView style={styles.container}>
-				<Slot />
-			</SafeAreaView>
-		</Provider>
+		<SafeAreaView style={styles.container}>
+			<StoreProvider>
+				<Stack screenOptions={{ headerShown: false }}>
+					<Stack.Screen name='(tabs)' />
+				</Stack>
+			</StoreProvider>
+		</SafeAreaView>
 	)
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: colors.white
+		flex: 1
 	}
 })
