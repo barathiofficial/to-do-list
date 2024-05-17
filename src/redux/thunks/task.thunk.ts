6121031cb0e7889a6c7e211c13b910b94c98bedd
Barathi <barathiofficial@gmail.com>
2024-05-17 12:51:22 +0530

    refactor: Update task thunk to fetch single task

--- src/redux/thunks/task.thunk.ts
+++ src/redux/thunks/task.thunk.ts
@@ -1,18 +1,8 @@
-import { $addTask, $deleteTask, $fetchTasks, $toggleTask, $updateTask } from '@db/services'
-import { createAsyncThunk } from '@reduxjs/toolkit'
-
-function createServiceThunk<R, A = void>(actionName: string, serviceFn: (args: A) => Promise<R>) {
-	return createAsyncThunk<R, A>(actionName, async (args, api) => {
-		try {
-			return await serviceFn(args)
-		} catch (error) {
-			return api.rejectWithValue(error)
-		}
-	})
-}
+import { $addTask, $deleteTask, $fetchTask, $fetchTasks, $updateTask } from '@db/services'
+import { createServiceThunk } from './utils'
 
 export const fetchTasks = createServiceThunk('tasks/fetchTasks', $fetchTasks)
 export const addTask = createServiceThunk('tasks/addTask', $addTask)
-export const toggleTask = createServiceThunk('tasks/toggleTask', $toggleTask)
 export const deleteTask = createServiceThunk('tasks/deleteTask', $deleteTask)
 export const updateTask = createServiceThunk('tasks/updateTask', $updateTask)
+export const fetchTask = createServiceThunk('tasks/fetchTask', $fetchTask)
