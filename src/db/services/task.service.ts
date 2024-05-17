15b3613ae8e01e24088f560a8c9d383030f8777c
Barathi <barathiofficial@gmail.com>
2024-05-17 12:50:18 +0530

    refactor: Update task service to use initDB function directly

--- src/db/services/task.service.ts
+++ src/db/services/task.service.ts
@@ -5,10 +5,9 @@ import { eq } from 'drizzle-orm'
 export type Task = typeof tasks.$inferSelect
 
 export async function $addTask(task: Omit<Task, 'id' | 'createdAt'>): Promise<Task> {
-	const db = await initDB()
+	const db = initDB()
 	const createdAt = new Date().toISOString()
 	const { lastInsertRowId: id } = await db.insert(tasks).values({ ...task, createdAt })
-
 	return {
 		...task,
 		id,
@@ -16,28 +15,28 @@ export async function $addTask(task: Omit<Task, 'id' | 'createdAt'>): Promise<Ta
 	}
 }
 
-export async function $fetchTasks(): Promise<Task[]> {
-	const db = await initDB()
+export function $fetchTasks(): Promise<Task[]> {
+	const db = initDB()
 	return db.select().from(tasks)
 }
 
 export async function $deleteTask(id: number) {
-	const db = await initDB()
-	db.delete(tasks).where(eq(tasks.id, id))
+	const db = initDB()
+	await db.delete(tasks).where(eq(tasks.id, id))
 	return id
 }
 
 export async function $updateTask(
 	task: Partial<Omit<Task, 'id' | 'createdAt'>> & Pick<Task, 'id'>
 ): Promise<Task> {
-	const db = await initDB()
+	const db = initDB()
 	const { id, ...data } = task
-	db.update(tasks).set(data).where(eq(tasks.id, id))
+	await db.update(tasks).set(data).where(eq(tasks.id, id))
 	return $fetchTask(id)
 }
 
 export async function $fetchTask(id: number): Promise<Task> {
-	const db = await initDB()
+	const db = initDB()
 	const $tasks = await db.select().from(tasks).where(eq(tasks.id, id))
 	return $tasks[0]
 }
