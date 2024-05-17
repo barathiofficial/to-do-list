2ca6550632e7488668761230f64b9a5b9221af47
Barathi <barathiofficial@gmail.com>
2024-05-17 12:50:48 +0530

    refactor: Add category and task slices to manage category and task data in Redux store

--- src/redux/slices/index.ts
+++ src/redux/slices/index.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
+import categorySlice from './category.slice'
 import taskSlice from './task.slice'
 
 const reducer = combineReducers({
-	[taskSlice.name]: taskSlice.reducer
+	[taskSlice.name]: taskSlice.reducer,
+	[categorySlice.name]: categorySlice.reducer
 })
 
 export default reducer
 
+export * from './category.slice'
 export * from './task.slice'
