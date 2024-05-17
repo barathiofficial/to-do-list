0461bbce8dbc0cd2dd13ec860524a6635aa1c791
Barathi <barathiofficial@gmail.com>
2024-05-17 12:49:57 +0530

    refactor: Update category service to use initDB function directly

--- src/db/services/category.service.ts
+++ src/db/services/category.service.ts
@@ -5,33 +5,34 @@ import { eq } from 'drizzle-orm'
 export type Category = typeof categories.$inferSelect
 
 export async function $addCategory(name: string): Promise<Category> {
-	const db = await initDB()
+	const db = initDB()
 	const { lastInsertRowId: id } = await db.insert(categories).values({ name })
 	return { id, name }
 }
 
 export async function $fetchCategories(): Promise<Category[]> {
-	const db = await initDB()
-	return db.select().from(categories)
+	const db = initDB()
+	const res = await db.select().from(categories)
+	return res
 }
 
 export async function $deleteCategory(id: number) {
-	const db = await initDB()
-	db.delete(categories).where(eq(categories.id, id))
+	const db = initDB()
+	await db.delete(categories).where(eq(categories.id, id))
 	return id
 }
 
 export async function $updateCategory(
 	category: Partial<Omit<Category, 'id' | 'createdAt'>> & Pick<Category, 'id'>
 ): Promise<Category> {
-	const db = await initDB()
+	const db = initDB()
 	const { id, ...data } = category
-	db.update(categories).set(data).where(eq(categories.id, id))
+	await db.update(categories).set(data).where(eq(categories.id, id))
 	return $fetchCategory(id)
 }
 
 export async function $fetchCategory(id: number): Promise<Category> {
-	const db = await initDB()
+	const db = initDB()
 	const $categories = await db.select().from(categories).where(eq(categories.id, id))
 	return $categories[0]
 }
