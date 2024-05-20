b2c4789feb6a9fc855c019b3fd5c864036536944
Barathi <barathiofficial@gmail.com>
2024-05-20 12:28:14 +0530

    refactor: Update StoreProvider to use type import for PropsWithChildren

--- src/redux/context/StoreProvider.tsx
+++ src/redux/context/StoreProvider.tsx
@@ -1,7 +1,8 @@
 import store from '@/redux/store'
+import type { PropsWithChildren } from 'react'
 import React from 'react'
 import { Provider } from 'react-redux'
 
-export default function StoreProvider(props: React.PropsWithChildren) {
+export default function StoreProvider(props: PropsWithChildren) {
 	return <Provider store={store}>{props.children}</Provider>
 }
