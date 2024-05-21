de709b6e711acdff26e55de746c8f90ac015fa8f
Barathi <barathiofficial@gmail.com>
2024-05-21 18:55:36 +0530

    refactor: Update colors.ts to use consistent variable names and organize color codes

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -1,207 +1,203 @@
-const white = '#FFFFFF'
-const black = '#000000'
+const WHITE = '#FFFFFF'
+const BLACK = '#000000'
 
-const teal500 = '#008080'
-const teal500b = '#009688'
-const teal700 = '#006666'
+const TEAL_500 = '#008080'
+const TEAL_600 = '#009688'
+const TEAL_700 = '#006666'
 
-const gray50 = '#F3F3F3'
-const gray50b = '#F0F0F0'
-const gray100 = '#F8F9FA'
-const gray200 = '#E0E0E0'
-const gray300 = '#CCCCCC'
-const gray400 = '#A9A9A9'
-const gray500 = '#9E9E9E'
-const gray600 = '#6C757D'
-const gray800 = '#212529'
-const gray900 = '#393939'
+const GRAY_50 = '#F3F3F3'
+const GRAY_100 = '#F8F9FA'
+const GRAY_200 = '#E0E0E0'
+const GRAY_300 = '#CCCCCC'
+const GRAY_400 = '#A9A9A9'
+const GRAY_500 = '#9E9E9E'
+const GRAY_600 = '#6C757D'
+const GRAY_800 = '#212529'
+const GRAY_900 = '#393939'
 
-const blue100 = '#007BFF20'
-const blue500 = '#007BFF'
-const blue500b = '#2196F3'
-const blue700 = '#0056B3'
+const BLUE_100 = '#007BFF20'
+const BLUE_500 = '#007BFF'
+const BLUE_600 = '#2196F3'
+const BLUE_700 = '#0056B3'
 
-const orange100 = '#FFA50020'
-const orange500 = '#FFA500'
-const orange700 = '#FF8C00'
+const ORANGE_100 = '#FFA50020'
+const ORANGE_500 = '#FFA500'
+const ORANGE_700 = '#FF8C00'
 
-const green100 = '#28A74520'
-const green500 = '#28A745'
-const green500b = '#4CAF50'
-const green700 = '#218838'
+const GREEN_100 = '#28A74520'
+const GREEN_500 = '#28A745'
+const GREEN_600 = '#4CAF50'
+const GREEN_700 = '#218838'
 
-const red500 = '#FF4500'
-const red500b = '#F44336'
-const red700 = '#B22222'
+const RED_500 = '#FF4500'
+const RED_600 = '#F44336'
+const RED_700 = '#B22222'
 
-const yellow300 = '#FFEB3B'
-const yellow500 = '#FFD700'
-const yellow700 = '#FFC107'
+const YELLOW_300 = '#FFEB3B'
+const YELLOW_500 = '#FFD700'
+const YELLOW_700 = '#FFC107'
 
-const purple500 = '#9C27B0'
-
-const deepPurple500 = '#673AB7'
-
-const brown500 = '#795548'
-
-const pink500 = '#E91E63'
-
-const indigo500 = '#3F51B5'
-
-const blueGray500 = '#607D8B'
+const VERY_LIGHT_GRAY = '#F0F0F0'
+const PURPLE_500 = '#9C27B0'
+const BROWN_500 = '#795548'
+const DEEP_PURPLE_500 = '#673AB7'
+const PINK_500 = '#E91E63'
+const INDIGO_500 = '#3F51B5'
+const BLUE_GRAY_500 = '#607D8B'
 
 export const colors = {
-	white,
-	black,
+	white: WHITE,
+	black: BLACK,
 	primary: {
-		teal: teal500,
-		darkTeal: teal700
+		teal: TEAL_500,
+		darkTeal: TEAL_700
 	},
 	accent: {
-		background: gray100,
+		background: GRAY_100,
 		text: {
-			primary: gray800,
-			secondary: gray600
+			primary: GRAY_800,
+			secondary: GRAY_600
 		}
 	},
 	button: {
 		addTask: {
-			teal: teal500,
-			darkTeal: teal700
+			teal: TEAL_500,
+			darkTeal: TEAL_700
 		}
 	},
 	border: {
-		lightGray: gray200
+		lightGray: GRAY_200
 	},
 	status: {
 		todo: {
 			bright: {
-				background: blue100,
-				foreground: blue500
+				background: BLUE_100,
+				foreground: BLUE_500
 			},
 			dark: {
-				background: blue700 + '20',
-				foreground: blue700
+				background: BLUE_700 + '20',
+				foreground: BLUE_700
 			}
 		},
 		progress: {
 			bright: {
-				background: orange100,
-				foreground: orange500
+				background: ORANGE_100,
+				foreground: ORANGE_500
 			},
 			dark: {
-				background: orange700 + '20',
-				foreground: orange700
+				background: ORANGE_700 + '20',
+				foreground: ORANGE_700
 			}
 		},
 		done: {
 			bright: {
-				background: green100,
-				foreground: green500
+				background: GREEN_100,
+				foreground: GREEN_500
 			},
 			dark: {
-				background: green700 + '20',
-				foreground: green700
+				background: GREEN_700 + '20',
+				foreground: GREEN_700
 			}
 		}
 	},
 	priority: {
 		high: {
-			bright: red500,
-			dark: red700
+			bright: RED_500,
+			dark: RED_700
 		},
 		medium: {
-			bright: orange500,
-			dark: orange700
+			bright: ORANGE_500,
+			dark: ORANGE_700
 		},
 		low: {
-			bright: yellow500,
-			dark: yellow700
+			bright: YELLOW_500,
+			dark: YELLOW_700
 		}
 	},
 	tabBar: {
-		background: gray100,
-		active: teal500,
-		inactive: gray600
+		background: GRAY_100,
+		active: TEAL_500,
+		inactive: GRAY_600
 	},
 	ripple: {
-		light: gray50,
-		dark: gray900
+		light: GRAY_50,
+		dark: GRAY_900
 	},
 	textInput: {
-		label: gray800,
-		error: red500,
-		placeholder: gray400,
+		label: GRAY_800,
+		error: RED_500,
+		placeholder: GRAY_400,
+		selection: TEAL_500 + '80',
 		border: {
-			default: gray300,
-			focused: teal500,
-			error: red500
+			default: GRAY_300,
+			focused: TEAL_500,
+			error: RED_500
 		},
 		background: {
-			default: white,
-			disabled: gray50b
+			default: WHITE,
+			disabled: VERY_LIGHT_GRAY
 		},
 		text: {
-			default: gray800,
-			disabled: gray600
+			default: GRAY_800,
+			disabled: GRAY_600
 		}
 	},
 	categories: {
 		shopping: {
-			background: green500b,
-			foreground: white
+			background: GREEN_600,
+			foreground: WHITE
 		},
 		education: {
-			background: blue500b,
-			foreground: white
+			background: BLUE_600,
+			foreground: WHITE
 		},
 		work: {
-			background: gray500,
-			foreground: white
+			background: GRAY_500,
+			foreground: WHITE
 		},
 		personal: {
-			background: purple500,
-			foreground: white
+			background: PURPLE_500,
+			foreground: WHITE
 		},
 		health: {
-			background: red500b,
-			foreground: white
+			background: RED_600,
+			foreground: WHITE
 		},
 		finance: {
-			background: yellow500,
-			foreground: black
+			background: YELLOW_500,
+			foreground: BLACK
 		},
 		entertainment: {
-			background: yellow300,
-			foreground: black
+			background: YELLOW_300,
+			foreground: BLACK
 		},
 		travel: {
-			background: teal500b,
-			foreground: white
+			background: TEAL_600,
+			foreground: WHITE
 		},
 		errands: {
-			background: orange500,
-			foreground: white
+			background: ORANGE_500,
+			foreground: WHITE
 		},
 		household: {
-			background: brown500,
-			foreground: white
+			background: BROWN_500,
+			foreground: WHITE
 		},
 		hobbies: {
-			background: deepPurple500,
-			foreground: white
+			background: DEEP_PURPLE_500,
+			foreground: WHITE
 		},
 		fitness: {
-			background: pink500,
-			foreground: white
+			background: PINK_500,
+			foreground: WHITE
 		},
 		social: {
-			background: indigo500,
-			foreground: white
+			background: INDIGO_500,
+			foreground: WHITE
 		},
 		other: {
-			background: blueGray500,
-			foreground: white
+			background: BLUE_GRAY_500,
+			foreground: WHITE
 		}
 	}
 }
