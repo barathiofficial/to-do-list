d3849a83557c20aa81ca1a05b26f523e800e1779
Barathi <barathiofficial@gmail.com>
2024-05-21 18:55:56 +0530

    remove redux

--- src/redux/context/StoreProvider.tsx
+++
@@ -1,8 +0,0 @@
-import store from '@/redux/store'
-import type { PropsWithChildren } from 'react'
-import React from 'react'
-import { Provider } from 'react-redux'
-
-export default function StoreProvider(props: PropsWithChildren) {
-	return <Provider store={store}>{props.children}</Provider>
-}
--- src/redux/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux'
-import type { AppDispatch, RootState } from '../store'
-
-export const useAppSelector = useSelector as <TSelected = unknown>(
-	selector: (state: RootState) => TSelected,
-	equalityFn?: (left: TSelected, right: TSelected) => boolean
-) => TSelected
-export const useAppDispatch = () => useDispatch<AppDispatch>()
